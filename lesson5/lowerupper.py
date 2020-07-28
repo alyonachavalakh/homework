0f716800c5b8b124376a9553f00e8ceb40f93cd7
alyonachavalakh <ol.chavalakh@gmail.com>
2020-07-28 12:12:02 +0300

    message

--- lesson5/lowerupper.py
+++ lesson5/lowerupper.py
@@ -1,3 +1,7 @@
+#Написать 2 функции. Первая функция будет принимать строку и приводить ее к нижнему регистру.
+#Вторая функция будет принимать строку и проводить ее к верхнему регистру.
+#После чего с помощью map применить ваши функции к списку строк. Отдельно каждую функцию к отдельному списку строк!
+
 def lower_case(string):
     return string.lower()
 
@@ -6,10 +10,11 @@ def upper_case(string):
     return string.upper()
 
 
-strings = ['ReD', 'YelLow', 'BlACk', 'WhiTe', 'GrEEn']
+strings = ['RED', 'YElLOW', 'BlACK', 'WHITE', 'GREEN']
+strings1 = ['red', 'yellow', 'black', 'white', 'green']
 
 new_list = list(map(lower_case, strings))
-new_list1 = list(map(upper_case, strings))
+new_list1 = list(map(upper_case, strings1))
 
 print(new_list)
 print(new_list1)
