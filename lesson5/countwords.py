afe56b2213aef9caad649ace00c9db3fa8870dd4
alyonachavalakh <ol.chavalakh@gmail.com>
2020-07-28 15:59:33 +0300

    message

---
+++ lesson5/countwords.py
@@ -0,0 +1,13 @@
+import re
+text = open('text.txt', 'r')
+text_string = text.read().lower()
+matching_words = re.findall(r'\b[a-z]{3,15}\b', text_string)
+frequency = {}
+
+for word in matching_words:
+    count = frequency.get(word,0)
+    frequency[word] = count + 1
+
+frequency_list = frequency.keys()
+for words in frequency_list:
+    print(words, frequency[words])
